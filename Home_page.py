406a604799b1d5d488631ef41781f955771abaf1
ssaybch <138187419+ssaybch@users.noreply.github.com>
2023-06-30 15:25:32 +0900

    Update Home_page.py

--- Home_page.py
+++ Home_page.py
@@ -11,9 +11,13 @@ def QEDcalculation(query):
     query로 SMILES를 받아 계산이나 해보자.
     """
     m = Chem.MolFromSmiles(query)
-    print("QED", round(QED.qed(m),3), "WLOGP", round(Chem.Crippen.MolLogP(m),2), sep='\t')
+    result = "QED" + str(round(QED.qed(m),3)) + "WLOGP" str(round(Chem.Crippen.MolLogP(m),2))
+    
+    return result
     
 
 input_string  = st.text_input("Please input interesting SMILES","CC(=C)C(O)=O")
+st.write("현재", input_string)
+
 if input_string == True:
     st.write(QEDcalculation(input_string)) #메타크릴산 독성.
