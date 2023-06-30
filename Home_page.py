4658ad434c0b8c2c8bf5fa464947483d301e03b1
ssaybch <138187419+ssaybch@users.noreply.github.com>
2023-06-30 15:22:45 +0900

    Update Home_page.py

--- Home_page.py
+++ Home_page.py
@@ -1,7 +1,19 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import QED
 
-st.write("Hello, world")
+def QEDcalculation(query):
+    """
+    간단한 QED 계산기. 
+    query로 SMILES를 받아 계산이나 해보자.
+    """
+    m = Chem.MolFromSmiles(query)
+    print("QED", round(QED.qed(m),3), "WLOGP", round(Chem.Crippen.MolLogP(m),2), sep='\t')
+    
 
-input_string  = st.text_input("Please input interesting SMILES","CCCC=CC")
+input_string  = st.text_input("Please input interesting SMILES",""CC(=C)C(O)=O")
+if input_string == True:
+    st.write(QEDcalculation(input_string)) #메타크릴산 독성.
